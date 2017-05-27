3589dcc7691d2d2eaf2a777e98d8a339c4309abc
terranc <wuchaoboy@gmail.com>
2017-05-27 23:31:02 +0800

    恢复jshint相关配置

--- Gruntfile.js
+++ Gruntfile.js
@@ -28,6 +28,16 @@ module.exports = function(grunt) {
             }
         },
 
+        jshint: {
+            all: {
+                src: ["src/**/*.js", "Gruntfile.js"],
+
+                options: {
+                    jshintrc: true
+                }
+            }
+        },
+
 
         uglify: {
             options: {
@@ -74,7 +84,7 @@ module.exports = function(grunt) {
 
     require("load-grunt-tasks")(grunt);
 
-    grunt.registerTask("default", ["sass", "cssmin",  "uglify", "copy"]);
+    grunt.registerTask("default", ["sass", "cssmin", "jshint", "uglify", "copy"]);
     grunt.registerTask("check", ["jshint"]);
     grunt.registerTask("debug", ["default", "watch"]);
 };
